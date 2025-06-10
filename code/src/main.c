2814cb7b08363320507859d6e308585306f94898
Muhammed ERDAL <m.erdal677@proton.me>
2025-06-10 16:53:33 +0200

    Modification de main.c

--- code/src/main.c
+++ code/src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 // Headers
 
 #include "include/config.h"
 #include "include/reseau.h"
+#include "include/trame.h"
 
 int main(void)
 {
@@ -17,6 +20,22 @@ int main(void)
   printf("Configuration chargée !\n");
 
   afficher_reseau_t(&rs);
+
+  trame_ethernet_t trame;
+  mac_address_t src = {{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E}};
+  mac_address_t dest = {{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}}; // Broadcast
+    
+  init_trame_ethernet(&trame, &src, &dest);
+  trame.type = htons(0x0800); // IPv4
+    
+  // Ajouter des données de test
+  const char *message = "Hello Ethernet!";
+  memcpy(trame.donnees, message, strlen(message));
+  trame.taille_donnees = strlen(message);
+    
+  afficher_trame(&trame);
+  printf("\n");
+  afficher_trame_hex(&trame);
 
   deinit_reseau_t(&rs);
 
